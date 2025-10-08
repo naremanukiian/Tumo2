a3490ee39f61cbd12456f2f2cb9640a469691d41
Your Name <you@example.com>
2025-10-08 21:50:38 +0400

    Updated Read_Button Arduino code

--- Read_Button/read_button.ino
+++ Read_Button/read_button.ino
@@ -1,15 +1,13 @@
-const int buttonPin = 2;
-const int ledPin = 13;
-int buttonState = 0;
+const int buttonPin = 2; 
+int buttonState = 0;     
 
-void setup()
-{
-  pinMode ( buttonPin, INPUT );
-  pinMode ( ledPin, OUTPUT );
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP); 
+  Serial.begin(9600);              
 }
 
-void loop ()
-{
-  buttonState = digitalRead (buttonPin);
-  digitalWrite ( ledPin, buttonState );
+void loop() {
+  buttonState = digitalRead(buttonPin); 
+  Serial.println(buttonState);          
+  delay(100);                           
 }
